1750718b4791f0711cc11533c4876ed9b99b2737
Andres A <acosta.andres.r@gmail.com>
2020-11-17 00:21:11 -0600

    Init files

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import './index.css';
+
+function Header() {
+  return (
+      <header>
+        <h1>Employee Directory</h1>
+        <p>
+          Click on carrots to filter by heading or use search box to narrow your results.
+        </p>
+      </header>
+  );
+}
+
+export default Header;
